062619f5bc40d24a719afe1d0ab64b557e467cc6
sssionek <semisio853@student.polsl.pl>
2021-05-31 22:45:37 +0200

    Add periodical refreshes of the process list

---
+++ caow/ViewModel/BaseClass/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace caow.ViewModel.BaseClass
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void TriggerPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- caow/ViewModel/ViewModelClass.cs
+++ caow/ViewModel/ViewModelClass.cs
@@ -5,20 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Timers;
 
 namespace caow.ViewModel
 {
     using BaseClass;
     using Model;
-    public class ViewModelClass : INotifyPropertyChanged
+    class ViewModelClass : ViewModelBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         ProcessHandler process = new ProcessHandler();
+        Timer processListTimer = new Timer(1000);
 
         public ObservableCollection<string> ProcessList
         {
             get { return new ObservableCollection<string>(process.GetProcesses()); }
+        }
+
+        private void UpdateProcessList(Object source, ElapsedEventArgs e)
+        {
+            TriggerPropertyChanged(nameof(ProcessList));
+        }
+
+        public ViewModelClass()
+        {
+            processListTimer.Elapsed += UpdateProcessList;
+            processListTimer.AutoReset = true;
+            processListTimer.Enabled = true;
         }
     }
 }
