99109679db327624818af33d8bab1671bf54b3d0
sssionek <semisio853@student.polsl.pl>
2021-06-01 16:25:21 +0200

    keep the selection in its place

--- caow/ViewModel/ViewModelClass.cs
+++ caow/ViewModel/ViewModelClass.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Input;
 using System.Collections.ObjectModel;
 using System.Timers;
 using System.Diagnostics;
@@ -16,15 +17,53 @@ namespace caow.ViewModel
     {
         ProcessHandler process = new ProcessHandler();
         Timer processListTimer = new Timer(1000);
-
+        private ObservableCollection<Process> processList;
         public ObservableCollection<Process> ProcessList
         {
-            get { return new ObservableCollection<Process>(process.GetProcesses()); }
+            get { processList = new ObservableCollection<Process>(process.GetProcesses()); return processList; }
         }
 
         private void UpdateProcessList(Object source, ElapsedEventArgs e)
         {
             TriggerPropertyChanged(nameof(ProcessList));
+        }
+
+        private Process selectedProcess;
+        public Process SelectedProcess
+        {
+            get { return selectedProcess; }
+            set
+            {
+                if (value != null)
+                    selectedProcess = value;
+            }
+        }
+        
+        private ICommand selectedProcessChanged = null;
+        public ICommand SelectedProcessChanged
+        {
+            get
+            {
+                if (selectedProcessChanged == null)
+                    selectedProcessChanged = new RelayCommand(
+                    (arg) => {
+                        if(selectedProcess != null)
+                            for (int i = 0; i < processList.Count; i++)
+                            {
+                                if (processList[i].Id == selectedProcess.Id)
+                                {
+                                    SelectedProcess = processList[i];
+                                    TriggerPropertyChanged(nameof(SelectedProcess));
+                                    return;
+                                }
+                            }
+                        selectedProcess = null;
+                        TriggerPropertyChanged(nameof(SelectedProcess));
+                    },
+                    (arg) => true);
+                return selectedProcessChanged;
+            }
+
         }
 
         public ViewModelClass()
